3aaa63504afcbc0a46d9a6d06073c462553722e8
Lee8955 <ST10516574@myemeris.edu.za>
2026-03-19 09:45:23 +0200

    my first commit

---
+++ app/src/main/java/com/example/charactermarkevalation/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.charactermarkevalation
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.charactermarkevalation.ui.theme.CharacterMarkEvalationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val gradeInput = findViewById<EditText>(R.id.editGrade)
+        val resultText = findViewById<TextView>(R.id.textResult)
+        val checkButton = findViewById<Button>(R.id.btnCheck)
+        val resetButton = findViewById<Button>(R.id.btnReset)
+
+        checkButton.setOnClickListener {
+            val grade = gradeInput.text.toString().uppercase()
+
+
+            if (grade == "A"){
+                resultText.text = "Excellent"
+
+            } else if (grade == "B"){
+                resultText.text = "Very Good"
+
+            } else if (grade == "C"){
+                resultText.text = "Good"
+
+            } else if (grade == "D") {
+                resultText.text = "Pass"
+
+            } else if (grade == "E"){
+                resultText.text = "Fail"
+
+            } else {
+                resultText.text = "Invalid Output"
+            }
+
+        }
+
+        //Reset Button
+        resetButton.setOnClickListener {
+            gradeInput.text.clear()
+            resultText.text = "Results will appear here"
+        }
+    }
+}
+
+
+
